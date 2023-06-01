5c66c1a4879c72751edb686e5a030d32ce05c64c
JoaoAlgeri <joaoalgeri@gmail.com>
2023-06-01 17:13:10 -0300

    Feat:functional layout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <Content>
-      <BrowserRouter>
-        <Routes>
+      <BrowserRouter>            
+        <Routes>          
         </Routes>
       </BrowserRouter>
     </Content>
