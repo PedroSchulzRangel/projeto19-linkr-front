b66eb36901626072e0b83fc8a9b44d8baae31cfd
oliveiratullio <tullio_sfmg2@hotmail.com>
2023-06-05 14:15:58 -0300

    Fix: App.js - SigInPage import path

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+<<<<<<< Updated upstream
 import SigIn from "./SigInPage.js/SigInPage";
 
+=======
+import SignUpPage from "./SignUpPage/SignUpPage";
+import TimeLine from "./TimelinePage/TimeLinePage";
+import SigIn from "./SigInPage/SigInPage"
+>>>>>>> Stashed changes
 import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
   return (
     <AuthProvider>
-      <Content>
+    
         <BrowserRouter>
           <Routes> 
           <Route path="/" element={<SigIn/>}/>
           </Routes>
         </BrowserRouter>
-      </Content>
+      
     </AuthProvider>
   );
 }
